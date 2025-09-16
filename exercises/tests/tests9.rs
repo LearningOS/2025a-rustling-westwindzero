36326c86e0cd0b78c37bb4fc7e80f107db695388
west wind <xfeiyv@gmail.com>
2025-09-16 22:03:13 +0800

    the last time

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,26 +27,38 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-
+//
+// extern "Rust" {
+//     fn my_demo_function(a: u32) -> u32;
+//     fn my_demo_function_alias(a: u32) -> u32;
+// }
+//
+// mod Foo {
+//     // No `extern` equals `extern "Rust"`.
+//     #[no_mangle]
+//     pub extern "Rust" fn my_demo_function(a: u32) -> u32 {
+//         a
+//     }
+//
+//     // 给别名
+//     #[no_mangle]
+//     pub extern "Rust" fn my_demo_function_alias(a: u32) -> u32 {
+//         my_demo_function(a)
+//     }
+// }
 extern "Rust" {
     fn my_demo_function(a: u32) -> u32;
+    #[link_name = "my_demo_function"]
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
 mod Foo {
     // No `extern` equals `extern "Rust"`.
     #[no_mangle]
-    pub extern "Rust" fn my_demo_function(a: u32) -> u32 {
+    fn my_demo_function(a: u32) -> u32 {
         a
     }
-
-    // 给别名
-    #[no_mangle]
-    pub extern "Rust" fn my_demo_function_alias(a: u32) -> u32 {
-        my_demo_function(a)
-    }
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
